11b58cc793bc12c3476add29cde92ffbe6b03e4e
Lucas Felipe <lucassantanafelipe@gmail.com>
2024-08-28 05:55:22 -0300

    Removing items from the Galvanotek list

--- inventory-management.py
+++ inventory-management.py
@@ -24,24 +24,37 @@ while True:
         pular_linha()
 
     elif escolha_opcao_geral == '2':
-        adicionar_produto_marca = input('Digite a marca do produto: ').upper().strip()
-        adicionar_produto_codigo = input('Digite o código do produto: ').upper().strip()
+        adicionar_produto_marca = input('Digite a marca do produto que deseja adicionar: ').upper().strip()
+        adicionar_produto_codigo = input('Digite o código do produto que será adicionado: ').upper().strip()
 
         print(f'Marca digitada: {adicionar_produto_marca}')
         print(f'Código digitado: {adicionar_produto_codigo}')
 
         if adicionar_produto_marca == 'GALVANOTEK' or adicionar_produto_marca == 'G':
             galvanotek.append(adicionar_produto_codigo)
-            print(f'Produto {adicionar_produto_codigo} adicionado à marca Galvanotek.')
+            print(f'Produto {adicionar_produto_codigo} adicionado à marca "Galvanotek".')
 
         if adicionar_produto_marca == 'HIPERPACK' or adicionar_produto_marca == 'H':
             hiperpack.append(adicionar_produto_codigo)
-            print(f'Produto {adicionar_produto_codigo} adicionado à marca HiperPack.')
+            print(f'Produto {adicionar_produto_codigo} adicionado à marca "HiperPack".')
         
         if adicionar_produto_marca == 'NIAGARA' or adicionar_produto_marca == 'N':
             hiperpack.append(adicionar_produto_codigo)
-            print(f'Produto {adicionar_produto_codigo} adicionado à marca Niagara.')
+            print(f'Produto {adicionar_produto_codigo} adicionado à marca "Niagara".')
 
+    elif escolha_opcao_geral == '3':
+        remover_produto_marca = input('Digite a marca do produto que deseja remover: ').upper().strip()
+        remover_produto_codigo = input('Digite o código do produto que será removido: ').upper().strip()
+        
+        print(f'Marca digitada: {remover_produto_marca}')
+        print(f'Código digitado: {remover_produto_codigo}')
+
+        if remover_produto_marca == 'GALVANOTEK' or remover_produto_marca == 'G':
+            galvanotek.remove(remover_produto_codigo)
+            print(f'Produto {remover_produto_codigo} removido da lista "Galvanotek".')
+
+
+    
     elif escolha_opcao_geral == '4':
         print('Saindo do programa.')
         break
