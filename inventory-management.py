db422d0e89a805d5b6e4d05be1b5c579b9ae13c1
Lucas Felipe <lucassantanafelipe@gmail.com>
2024-09-23 06:22:36 -0300

    Creating the table 'Products' in database

--- inventory-management.py
+++ inventory-management.py
@@ -2,3 +2,11 @@ import sqlite3
 
 connect = sqlite3.connect('inventory.db')
 cursor = connect.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+        marca TEXT NOT NULL,
+        codigo TEXT NOT NULL)
+''')
+
+connect.commit()
