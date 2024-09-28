b5370cd25b8a75d03d71d0d4933a5222e127c7f9
Lucas Felipe <lucassantanafelipe@gmail.com>
2024-09-28 18:05:40 -0300

    Handling exceptions

--- inventory-management.py
+++ inventory-management.py
@@ -1,28 +1,27 @@
 import sqlite3
 
-connect = sqlite3.connect('inventory.db')
-cursor = connect.cursor()
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Products(
-        marca TEXT NOT NULL,
-        codigo TEXT NOT NULL)
-''')
-
-connect.commit()
-
-products = {
-    'galvanotek': ['GA90', 'G695', 'G697'],
-    'niagara': ['MUD1', 'MUD2', 'MUD3'],
-    'hiperpack': ['H742', 'H10', 'H20'],
-    'goldpack': ['3L', '5L', '7L', '12L', 'PESC15', '17L', '28L']
-}
-
-for brand, codes in products.items(): 
-    for code in codes:
-        cursor.execute('''
-            INSERT INTO Products (marca, codigo) VALUES (?, ?)
-        ''', (brand, code))
+try:
+    with sqlite3.connect('inventory.db') as connect:
+        cursor = connect.cursor()
         
-connect.commit()
-connect.close()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Products(
+                marca TEXT NOT NULL,
+                codigo TEXT NOT NULL)
+        ''')
+        
+        products = {
+            'galvanotek': ['GA90', 'G695', 'G697'],
+            'niagara': ['MUD1', 'MUD2', 'MUD3'],
+            'hiperpack': ['H742', 'H10', 'H20'],
+            'goldpack': ['3L', '5L', '7L', '12L', 'PESC15', '17L', '28L']
+        }
+
+        for brand, codes in products.items(): 
+            for code in codes:
+                cursor.execute('''
+                    INSERT INTO Products (marca, codigo) VALUES (?, ?)
+                ''', (brand, code))
+
+except sqlite3.Error as e:
+    print(f"An error occurred: {e}")
